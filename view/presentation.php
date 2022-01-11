e5c087ea05e0627cc5c0ded5400dd2d13a6445e6
luvumbu <luvumbu.n@gmail.com>
2022-01-11 11:33:29 +0100

    ok

--- view/presentation.php
+++ view/presentation.php
@@ -1,16 +1,12 @@
 <div class="section-1" id="section-1">
  <p class="section-1-text police1">
-    Bienvenue sur le site bar a tresse votre salon de coiffure à lille.
-     Bar a tresse est le spécialiste de vos cheveux. de votre look. 
-     La beauté naturelle se trouve en chacun de nous, il faut seulement la reconnaître et la libérer. 
-     Certaines études ont démontré qu'une visite chez le coiffeur remontait le moral ! Nous l'avions tous remarqué.
-      Une mine basse, des yeux tristes, un vague à l'âme, une envie de changement,
-       un je-ne-sais-quoi de routine qui nous ennuie, direction : 
-       le coiffeur Notre Salon Etre créateur de Beauté et de Bien-être est notre vocation profonde: 
-       votre visite chez nous doit donc être l’occasion d’un moment de plaisir unique. L’équipe Nous proposons
-        à notre clientèle une palette de services variés, une écoute et un conseil professionnels et personnalisés.
-         Beauté Nous nous réjouissons de pouvoir vous accueillir dans notre univers.
-          Laissez-vous dorloter par nos experts qui vous prodigueront de nombreux conseils en matière de beauté.
+    Bienvenue sur le site 'bar à tresses', votre salon de béaute à lille.
+     Votre beauté est notre seul mot d'ordre. Nous proposons à notre clientèle une palette de services variés, 
+     une écoute et un conseil professionnel et personnalisé. Chez nous, vous pourrez vous faire coiffer, épiler, 
+     poser des ongles, poser des extensions de cils... Nous sommes là pour vous chouchouter, et pour que vous 
+     passiez un moment de détente et de bien-être absolu. 
+     Laissez-vous dorloter par nos experts qui vous prodigueront de nombreux conseils en matière de beauté.
+               
  </p>
  
   </div>
